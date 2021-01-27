0c7167e41ae893e1dd77a6a566edd3d715335b52
Oumar Mimouni <omimouni@student.1337.ma>
2021-01-27 22:11:19 +0100

    update render

--- src/mrt_engine/render.c
+++ src/mrt_engine/render.c
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 17:00:07 by omimouni          #+#    #+#             */
-/*   Updated: 2021/01/27 19:18:44 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/01/27 22:11:08 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,34 +18,24 @@ extern t_conf	*g_conf;
  * @todo Add render progress
  **/
 
-void static		show_progress(size_t i, size_t j)
-{
-	size_t	progress;
-
-	progress = g_conf->height * g_conf->width;
-	i++;
-	j++;
-	printf("\r\033[0;32m  👾 Progress: %ld%%",((i * j) / progress) * 100);
-}
-
 void	mrt_render(unsigned char type)
 {
 	size_t	i;
 	size_t	j;
 
-	printf("\n* 🖥  Rendering Scene \n\n");
+	printf("< 🖥  Rendering Scene >\n");
+	printf("-> 👾 \033[0;32mProgress ...");
 	i = 0;
 	while (i < g_conf->width)
 	{
 		j = 0;
 		while (j < g_conf->height)
 		{
-			show_progress(i, j);
 			if (type == MRT_RENDER_WINDOW)
 				mrt_put_pixel(i, j, 0xFFFFFF);
 			j++;
 		}
 		i++;
 	}
-	printf("...\033[0m ✅\n");
-	printf("\n* 🤵 waiting for the next command. \n"); }
+	printf("\033[0m ✅\n");
+	printf("< 🤵 waiting for the next command >\n"); }
