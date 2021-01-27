4e42d1727fecabbfb1a1f268cdb7f665e8a8d27a
Oumar Mimouni <omimouni@student.1337.ma>
2021-01-27 22:01:02 +0100

    init fun

--- src/mrt_parser/init.c
+++ src/mrt_parser/init.c
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 17:55:43 by omimouni          #+#    #+#             */
-/*   Updated: 2021/01/25 23:03:37 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/01/27 21:59:40 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,4 @@ void	mrt_init(void)
 	g_conf->is_save = 0;
 	g_conf->height = 1000;
 	g_conf->width = 1000;
-	
 }
