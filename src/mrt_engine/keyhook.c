c8a84e7e232e4d8b4fdfa2cb4dbb5f892bd4dc2b
Oumar Mimouni <omimouni@student.1337.ma>
2021-02-01 10:00:32 +0100

    Clean up engine

--- src/mrt_engine/keyhook.c
+++ src/mrt_engine/keyhook.c
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 18:01:54 by omimouni          #+#    #+#             */
-/*   Updated: 2021/02/01 09:26:54 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/02/01 10:00:09 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,6 @@ void static mrt_quit()
 
 void static	mrt_next_cam()
 {
-	mlx_string_put(g_conf->mlx.ptr, g_conf->mlx.win,
-	g_conf->width / 2 - 60, g_conf->height / 2, 0xFFFFFF, "Rendering new scene");
-	sleep(3);
-	printf("-> Selecting next camera\n");
-	g_conf->current_camera = camera_new_(vec3_new(0, 0, 0), vec3_new(0, 0, -1), 80);
-	mrt_render();
-	mrt_update_window();
 }
 
 int	mrt_key_handler(int keycode)
--- src/mrt_engine/render/render.c
+++ src/mrt_engine/render/render.c
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 17:00:07 by omimouni          #+#    #+#             */
-/*   Updated: 2021/02/01 09:11:40 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/02/01 09:58:43 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ extern t_conf	*g_conf;
 
 void	mrt_render(void)
 {
-	printf("< 🖥  Rendering Scene >\n");
-	printf("-> 👾 \033[0;32mProgress ...");
+	printf("🖥  Rendering Scene\n");
+	printf("👾 \033[0;32mProgress ...");
 	mrt_render_loop();
 	printf("\033[0m ✅\n");
-	printf("< 🤵 waiting for the next command >\n");
+	printf("\n🤵 waiting for the next command\n");
 }
