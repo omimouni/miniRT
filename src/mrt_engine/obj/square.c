5f0ba25335ba779d33f8ad1f2b89cf073802fde7
Oumar Mimouni <omimouni@student.1337.ma>
2021-02-12 17:13:46 +0100

    square equation

--- src/mrt_engine/obj/square.c
+++ src/mrt_engine/obj/square.c
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 09:36:53 by omimouni          #+#    #+#             */
-/*   Updated: 2021/02/12 12:51:34 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/02/12 17:13:14 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,22 +48,35 @@ double		mrt_square_intersection(t_mrt_ray *ray, t_object *obj)
 	t_vector3	e2;
 
 	u = vec3_cross(sq->normal, mrt_current_camera()->normal);
+	u = vec3_normalize(u);
 	v = vec3_cross(u, sq->normal);
-	ux = vec3_mult(3, u);
-	vx = vec3_mult(3, v);
-	pa = vec3_add(vec3_add(sq->point, ux), vx);
-	e1 = vec3_add(pa, vec3_mult(-6, u));
-	e2 = vec3_add(pa, vec3_mult(-6, v));
+	v = vec3_normalize(v);
+	ux = vec3_mult(sq->size / 2, u);
+	vx = vec3_mult(sq->size / 2, v);
+	pa = vec3_add(vec3_add(sq->point, vx), ux);
+	e1 = vec3_add(pa, vec3_mult(-sq->size, u));
+	e2 = vec3_add(pa, vec3_mult(-sq->size, v));
 
-	// Calculate Plane intersection	
-	k1 = vec3_dot(vec3_sub(ray->origin, sq->point), sq->normal);
+	// Calculate Plane intersection
+	k1 = vec3_dot(vec3_sub(ray->origin, pa), sq->normal);
 	k2 = vec3_dot(ray->direction, sq->normal);
 	if (!k2)
 		return (INFINITY);
 	t = -k1 / k2;
 
 	// Limit square
-	// k1 = 
-	
-	return (t);
+	t_point3	tp;
+	t_vector3	vv;
+	double		proj1;
+	double		proj2;
+
+	tp = mrt_ray_point(t, ray);
+	vv = vec3_sub(tp, pa);
+
+	proj1 = vec3_dot(vv, e1) / sq->size;
+	proj2 = vec3_dot(vv, e2) / sq->size;
+
+	if ((proj1 < sq->size && proj1 > 0) && (proj2 < sq->size && proj2 > 0))
+		return (t);
+	return (INFINITY);
 }
