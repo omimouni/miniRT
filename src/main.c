276b1a7f0760cc6b91367128dffccfec3c18f69a
Oumar Mimouni <omimouni@student.1337.ma>
2021-02-17 12:10:07 +0100

    bonus activate

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 16:15:07 by omimouni          #+#    #+#             */
-/*   Updated: 2021/02/17 11:46:47 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/02/17 12:08:14 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(int argc, char **argv)
 {
 	mrt_init();
 	mrt_parser(argc, argv);
-	g_conf->is_bonus = 0;
+	g_conf->is_bonus = 1;
 	if(g_conf->cameras == NULL)
 		mrt_trigger_error(10);
 	if (g_conf->is_save)
