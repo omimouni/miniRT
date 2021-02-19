429eb2f61580ad513b1334b03318d8b9b0f07a57
Oumar Mimouni <omimouni@student.1337.ma>
2021-02-19 15:47:32 +0100

    working on bmp

--- src/mrt_engine/bmp.c
+++ src/mrt_engine/bmp.c
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 16:56:26 by omimouni          #+#    #+#             */
-/*   Updated: 2021/02/17 17:21:09 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/02/19 15:47:23 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 
 extern	t_conf	*g_conf;
 
-void	mrt_bmp_header(int fd)
+void				bit_insert(unsigned char *ar, int val)
 {
-	ft_putstr_fd("BM", fd);
-	ft_putstr_fd("54054", fd);
+	int	i;
+
+	i = 0;
+	while (i < 4)
+		ar[i] = (unsigned char)(val >> (8 * i++));
+}
+
+unsigned	char	*bmp_header(void)
+{
+	unsigned char	*header;
+
+	header = malloc(sizeof(unsigned char) * 14);
+	header[0] = 'B';
+	header[1] = 'M';
+	bit_insert(&header[2], 4 * g_conf->width * g_conf->height + 54);
+	header[10] = 54;
+	return (header);
 }
