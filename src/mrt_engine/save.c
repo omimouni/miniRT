6a287698546fe9aeccd2192ac85f7bc19462ec17
Oumar Mimouni <omimouni@student.1337.ma>
2021-02-20 22:34:02 +0100

    fixed bmp file

--- src/mrt_engine/save.c
+++ src/mrt_engine/save.c
@@ -6,7 +6,7 @@
 /*   By: omimouni <omimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 16:24:39 by omimouni          #+#    #+#             */
-/*   Updated: 2021/02/20 16:38:14 by omimouni         ###   ########.fr       */
+/*   Updated: 2021/02/20 22:33:01 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	mrt_save_image(void)
 	int				fd;
 	unsigned char	*color[3];
 	char			*filename;
+	int				*image;
 	unsigned char	*hrds[2];
 	int				x;
 	int				y;
@@ -30,16 +31,17 @@ void	mrt_save_image(void)
 	filename = ft_strjoin(WINDOW_TITLE, ".bmp");
 	fd = open(filename, O_CREAT | O_WRONLY, 777);
 	hrds[0] = bmp_header();
-	hrds[1] = bmp_header();
+	hrds[1] = bmp_header_info();
 	write(fd, hrds[0], 14);
 	write(fd, hrds[1], 40);
 	y = g_conf->height + 1;
+	image = ((int *)g_conf->mlx.img.addr);
 	while (y-- > 0)
 	{
 		x = g_conf->width * y - g_conf->width;
-		while (x < (int)g_conf->width * y)
+		while (x < g_conf->width * y)
 		{
-			bit_insert((unsigned char *)color, 0xFF0000);
+			bit_insert((unsigned char *)color, image[x]);
 			write(fd, color, 3);
 			x++;
 		}
